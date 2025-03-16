3dcb2bc92a5559a850cbbcd1c54664fab80a9f1d
devkirti28 <63730305+devkirti28@users.noreply.github.com>
2025-03-17 02:40:41 +0530

    Time: 19 ms (43.08%) Space: 29 MB (90.05%)

---
+++ 0475-heaters/heaters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool canCover(vector<int>& houses, vector<int>& heater, int r){
+        int houseIndex=0;
+        int heaterIndex=0;
+
+        while(houseIndex<houses.size() && heaterIndex<heater.size()){
+            if(heater[heaterIndex]-r <= houses[houseIndex] && heater[heaterIndex]+r >= houses[houseIndex]){
+                houseIndex++;
+            }else{
+                heaterIndex++;
+            }
+        }
+        return houseIndex==houses.size();
+    }
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        int l=0; int r=INT_MAX;
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        int ans=0;
+        while(l<r){
+            int mid = (l+r)/2;
+            if(canCover(houses,heaters,mid)){
+                r=mid;
+                ans=mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
