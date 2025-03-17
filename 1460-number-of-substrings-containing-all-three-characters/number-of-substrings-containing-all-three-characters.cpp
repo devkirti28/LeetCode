863a4d9b434d0bb4f6a545b234d383986de85c29
devkirti28 <63730305+devkirti28@users.noreply.github.com>
2025-03-17 21:26:06 +0530

    Time: 18 ms (38.24%) Space: 11.1 MB (64.63%)

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,10 +1,6 @@
 class Solution {
-    bool validSubstring( unordered_map<char,int>& freq){
-        return (freq['a']>0 && freq['b']>0 && freq['c']>0);
-    }
 public:
     int numberOfSubstrings(string s) {
-        unordered_map<char,int> freq;
         vector<int> last = {-1,-1,-1};
         int count=0;
         for(int i=0;i<s.size();i++){
