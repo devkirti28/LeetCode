92e294a01f17a91a625f9692ded7e6cac5c53c17
devkirti28 <63730305+devkirti28@users.noreply.github.com>
2025-03-17 17:43:33 +0530

    Time: 41 ms (5.03%) Space: 20.2 MB (34.89%)

---
+++ 0207-course-schedule/course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adlist(numCourses);
+        vector<int> indegree(numCourses,0);
+        for(auto it: prerequisites){
+            adlist[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0){
+                q.push(i);
+                cout<<i<<endl;
+            }
+        }
+
+        vector<int> topo;
+
+        while(!q.empty()){
+            int node = q.front();q.pop();
+            topo.push_back(node);
+            for(auto next : adlist[node]){
+                indegree[next]--;
+                if(indegree[next]==0){
+                    q.push(next);
+                }
+            }
+        }
+        for(auto it:topo){
+            cout<<it<<endl;
+        }
+        return (topo.size()==numCourses);
+    }
+};
