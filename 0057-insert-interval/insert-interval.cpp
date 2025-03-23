7d80daf6f6c197360edb09ef455ce308dd2a81e4
deddefault <63730305+devkirti28@users.noreply.github.com>
2025-03-24 04:44:04 +0530

    Time: 0 ms (100.00%) Space: 21.7 MB (45.81%)

---
+++ 0057-insert-interval/insert-interval.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int newstart = newInterval[0];
+        int newend = newInterval[1];
+        int n=intervals.size();
+        
+        vector<vector<int>> answer;
+
+        if(n==0){
+            answer.push_back({newstart,newend});
+            return answer;
+        }
+        int i=0;
+        // all non overlapping intervals before new
+        while(i<n && intervals[i][1] < newstart){
+            answer.push_back({intervals[i][0],intervals[i][1]});
+            i++;
+        }
+        // need to add new interval
+        int currentstart;
+        if(i<n){
+            currentstart = min(intervals[i][0],newstart);
+        }else{
+            currentstart = newstart;
+        }
+        int  currentend  = newend;
+        while(i<n && newend >= intervals[i][0]){
+            currentend = max(newend,intervals[i][1]);
+            i++;
+        }
+        answer.push_back({currentstart, currentend});
+        // add remaining intervals
+        while(i<n){
+            answer.push_back({intervals[i][0],intervals[i][1]});
+            i++;
+        }
+        return answer;
+
+    }
+};
