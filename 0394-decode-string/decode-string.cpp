f840d9d519f2f8cfc924508b0930268b1df9fc48
deddefault <63730305+devkirti28@users.noreply.github.com>
2025-04-10 18:42:44 +0530

    Time: 2 ms (27.09%) Space: 14 MB (6.56%)

--- 0394-decode-string/decode-string.cpp
+++ 0394-decode-string/decode-string.cpp
@@ -1,83 +1,44 @@
 class Solution {
-private:
-    int findLength(int i, char ch, string& s){
-        int length=0;
-        while(i<s.size()){
-            if(s[i++]==ch)break;
-            length++;
-        }
-        return length;
-    }
-    int findLengthOfChars(int i, string& s){
-        int length=0;
-        while(i<s.size()){
-            if(s[i]==']' || (s[i]-'0' >= 0 && s[i]-'9'<= 9) )break;
-            i++;
-            length++;
-        }
-        return length;
-    }
-    string decodeSubString(int num, string s){
-        if(s.size()==1){
-            char ch = s[0];
-            string temp(num,ch);
-            return temp;
-        }
-        string result="";
-        while(num--){
-            result+=s;
-        }
-        return result;
-    }
 public:
     string decodeString(string s) {
+        stack<char> st;
 
-        // string num="", chars="", result="";
-        // int i=0;
-        // while(i<s.size()){
-        //     // extract till [
-        //     int length = findLength(i, '[', s);
-            
-        //     num = s.substr(i,length);
-        //     i+= length+1;
+        int n=s.size();
+        int i=0;
 
-        //     length = findLength(i, ']', s);
-        //     chars = s.substr(i, length);
-        //     i+= length+1;
-        //     result+= decodeSubString(num , chars);
-        // }
-        // return result;
-
-        stack<string> st;
-        for(int i=0; i<s.length(); i++) {
-            if(s[i] == ']') {
-                string ans = "";
-                while(st.top() != "[") {
-                    ans = st.top() + ans;
+        while(i<n){
+            if(s[i]==']'){
+                string repeatString = "";
+                while(!st.empty() && st.top()!='['){
+                    repeatString = st.top() + repeatString;
                     st.pop();
                 }
                 st.pop();
-                string times = "";
-                while(!st.empty() && isdigit(st.top()[0])) {
-                    times = st.top() + times;
+                string repeatNumber = "";
+                while(!st.empty() && isdigit(st.top())){
+                    repeatNumber = st.top() + repeatNumber;
                     st.pop();
                 }
-                int to_times = stoi(times);
-                string ret = "";
-                while(to_times--) {
-                    ret = ans + ret;
+                int repeats = stoi(repeatNumber);
+                string repeatedString = "";
+                while(repeats--){
+                    repeatedString+=repeatString;
                 }
-                st.push(ret);
-            } else {
-                string temp = s.substr(i,1);
-                st.push(temp);
+                int j=0;
+                int tempLength=repeatedString.size();
+                while(j<tempLength){
+                    st.push(repeatedString[j++]);
+                }
+            }else{
+                st.push(s[i]);
             }
+            i++;
         }
-        string res = "";
-        while(!st.empty()) {
-            res = st.top() + res;
+        string result="";
+        while(!st.empty()){
+            result = st.top() + result;
             st.pop();
         }
-        return res;
+        return result;
     }
 };
